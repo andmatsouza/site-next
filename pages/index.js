f24a8166450947d92c553ce1f84f59f240eb63cc
andmatsou <andmatsou@gmail.com>
2022-06-02 20:19:08 -0300

    feat: adicionado font-awesome

--- pages/index.js
+++ pages/index.js
@@ -5,16 +5,19 @@ function Home() {
   return (
     <div>
       <Head>
-      <meta charSet="utf-8" />
-        <meta name="robots" content="index, follow"/>
-        <meta name="description" content="Site sobre material de limpeza"/>
-        <meta name="author" content="Anderson - Telelimp"/>
+        <meta charSet="utf-8" />
+        <meta name="robots" content="index, follow" />
+        <meta name="description" content="Site sobre material de limpeza" />
+        <meta name="author" content="Anderson - Telelimp" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"
+        />
         <title>Telelimp</title>
       </Head>
       <Menu />
       <p>Meu site sobre...</p>
-      
     </div>
   );
 }
