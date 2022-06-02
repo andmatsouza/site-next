55668d75f3eb2317fa1abc6f3ff4b1ab478a8c43
andmatsou <andmatsou@gmail.com>
2022-06-02 18:47:30 -0300

    feat: Como criar um cabeçalho no Next.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,18 @@
+import Head from "next/head";
+
 function Home() {
-  return <div>Welcome to Next.js!</div>
+  return (
+    <div>
+      <Head>
+        <meta name="robots" content="index, follow"/>
+        <meta name="description" content="Site sobre material de limpeza"/>
+        <meta name="author" content="Anderson - Telelimp"/>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <title>Telelimp</title>
+      </Head>
+      Meu site sobre...
+    </div>
+  );
 }
 
 export default Home;
