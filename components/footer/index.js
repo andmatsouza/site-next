2d12f9b802cafef964ae86800e254162cd51287d
andmatsou <andmatsou@gmail.com>
2022-06-03 22:14:28 -0300

    feat: adicionado componente footer

---
+++ components/footer/index.js
@@ -0,0 +1,11 @@
+import Styles from './styles.module.css';
+export const Footer = () => {
+  return (
+  <div>
+    <footer className={Styles.footer}>
+      <span>
+        Created By <a href="https://telelimp.com.br">Telelimp</a>
+      </span>
+    </footer>
+  </div>
+)};
