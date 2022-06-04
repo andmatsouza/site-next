3b7bb5cc6bede3e9dd2cdefa9db0c83bcc91572e
andmatsou <andmatsou@gmail.com>
2022-06-03 22:11:15 -0300

    feat: refatoração da página contato, adicionado componentes.

--- pages/contato.js
+++ pages/contato.js
@@ -1,17 +1,12 @@
-import Head from "next/head";
-import Menu from "../components/Menu";
+import { PageHead, Menu} from "../components";
 
 function Contato() {
   return(
     <div>
-      <Head>
-        <meta charSet="utf-8" />
-      <meta name="robots" content="index, follow"/>
-        <meta name="description" content="Contato com a empresa Telelimp"/>
-        <meta name="author" content="Anderson - Telelimp"/>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <title>Contato - Telelimp</title>
-      </Head>
+       <PageHead
+      title="Contato - Telelimp"
+      description="Contato com a empresa Telelimp"
+    ></PageHead>      
       <Menu />
       <p>Página de contato</p>
     </div>
