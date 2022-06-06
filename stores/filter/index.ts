551777b1f3571ab6963702f11951fe2eb02e3d4a
andmatsou <andmatsou@gmail.com>
2022-06-06 18:24:48 -0300

    feat: adicionado toggleModal no store

--- stores/filter/index.ts
+++ stores/filter/index.ts
@@ -1,17 +1,17 @@
 import create from "zustand";
 
+interface BearState {
+  bears: number;
+  isOpen: boolean;  
+}
 
-
-export const useFilter = create((set) => {
-  const initialState = {
-    isOpen: false,
-  };     
-
-
+export const useStore = create<BearState>((set) => {
+  
   return {
-    state: {
-      ...initialState,
-    },
-    toggleModal: () => set(({ state }:any) => {state.isOpen = !state.isOpen;}),
-  };
+  bears: 0,
+  isOpen: false,
+  toggleModal: () => set((state) => ({ isOpen: !state.isOpen})),
+  increasePopulation: () => set((state) => ({ bears: state.bears + 1 })),
+  removeAllBears: () => set({ bears: 0 }),
+}
 });
