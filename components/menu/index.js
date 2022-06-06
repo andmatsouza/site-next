14d2e075b04fdbf0447abe6094f14043842ef0f7
andmatsou <andmatsou@gmail.com>
2022-06-06 18:28:42 -0300

    feat: refatorado o componente menu p funcionar ssem o jquery

--- components/menu/index.js
+++ components/menu/index.js
@@ -1,34 +1,60 @@
-import Styles from './styles.module.css';
+import Styles from "./styles.module.css";
+import create from "zustand";
+
+const useFilter = create((set) => {
+  const initialState = {
+    isOpen: false,
+  };
+
+  return {
+    state: {
+      ...initialState,
+    },
+    toggleModal: () =>
+      set(({ state }) => {
+        state.isOpen = !state.isOpen;
+      }),
+  };
+});
 
 export const Menu = () => {
+  const { isOpen } = useFilter((store) => store.state);
+  const { toggleModal } = useFilter((store) => store);
   return (
-  <div>
-    <nav className={Styles.navbar}>
-      <div className={Styles.maxwidth}>
-        <div className={Styles.logo}>
-          <a href="/">Telelimp</a>
+    <div>
+      <nav className={Styles.navbar}>
+        <div className={Styles.maxwidth}>
+          <div className={Styles.logo}>
+            <a href="/">Telelimp</a>
+          </div>
+
+        <ul className={ isOpen ? Styles.menumodal : Styles.menu}>
+            <li>
+              <a href="/" className={Styles.menubtn}>
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="sobre-empresa" className={Styles.menubtn}>
+                Sobre Empresa
+              </a>
+            </li>
+            <li>
+              <a href="contato" className={Styles.menubtn}>
+                Contato
+              </a>
+            </li>
+          </ul>
+
+          <div className={Styles.menubtn}>
+            <i
+              id="test"
+              className={ isOpen ? "fa-solid fa-xmark" : "fas fa-bars"}
+              onClick={toggleModal}
+            ></i>
+          </div>
         </div>
-        <ul className={Styles.menu}>
-          <li>
-            <a href="/" className={Styles.menubtn}>
-              Home
-            </a>
-          </li>
-          <li>
-            <a href="sobre-empresa" className={Styles.menubtn}>
-              Sobre Empresa
-            </a>
-          </li>
-          <li>
-            <a href="contato" className={Styles.menubtn}>
-              Contato
-            </a>
-          </li>
-        </ul>
-        <div className={Styles.menubtn}>
-          <i className="fas fa-bars"></i>
-        </div>
-      </div>
-    </nav>    
-  </div>
-)};
+      </nav>
+    </div>
+  );
+};
